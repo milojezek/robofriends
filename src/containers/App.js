42877f36bed38fc511d65e1a7662505538d7b4ad
Miloslav Jezek <miloslav.jezek@fpt.sk>
2023-10-16 12:57:20 +0200

    Use hooks in App.js and delete hardcoded list of robots

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,64 +1,52 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBox from "../components/SearchBox";
 import CardList from "../components/CardList";
-import { robots } from "../Robots";
-import Scroll from "../components/Scroll"
+import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
-import "./App.css"
+import "./App.css";
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+  const [count, setCount] = useState(0);
 
-        console.log("constructor");
-    }
+  // useEffect gets run automatically every time the function App gets run
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
 
-    componentDidMount() {
+    console.log(count);
+  }, [count]); // only run if count changes
 
-        // Make request to server
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
 
-        console.log("component did mount");
-    }
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
+  console.log("render");
 
-        // console.log(filteredRobots);
-    }
-
-    render() {
-        const { robots, searchfield } = this.state;
-
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-
-        console.log("render");
-
-
-        // Condition just to show 'Loading' if it takes too much time to load the data
-        // (robots.length === 0) can be written as (robots.length) in JS
-        return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-    }
+  // Condition just to show 'Loading' if it takes too much time to load the data
+  // (robots.length === 0) can be written as (robots.length) in JS
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <button onClick={() => setCount(count + 1)}>Click Me!</button>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
